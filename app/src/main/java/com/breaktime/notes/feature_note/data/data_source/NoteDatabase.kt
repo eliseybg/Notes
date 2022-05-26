5f71cbf43ebe0efcec79ee4e6f3fa428f48e6098
Elisey Budnitsky <bgs6544@gmail.com>
2022-05-26 19:37:04 +0300

    feat: setup di

--- app/src/main/java/com/breaktime/notes/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/breaktime/notes/feature_note/data/data_source/NoteDatabase.kt
@@ -4,7 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.breaktime.notes.feature_note.domain.model.Note
 
-@Database(entities = [Note::class], exportSchema = false, version = 1)
+@Database(entities = [Note::class], version = 1)
 abstract class NoteDatabase: RoomDatabase() {
     abstract val noteDao: NoteDao
+
+    companion object {
+        const val DATABASE_NAME = "notes_db"
+    }
 }
