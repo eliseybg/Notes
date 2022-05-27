4017e07a2cbeeea87cce48d3c8159b249704da59
Elisey Budnitsky <bgs6544@gmail.com>
2022-05-27 19:56:38 +0300

    feat: add view model for add edit note

--- app/src/main/java/com/breaktime/notes/di/AppModule.kt
+++ app/src/main/java/com/breaktime/notes/di/AppModule.kt
@@ -5,9 +5,7 @@ import androidx.room.Room
 import com.breaktime.notes.feature_note.data.data_source.NoteDatabase
 import com.breaktime.notes.feature_note.data.repository.NoteRepositoryImpl
 import com.breaktime.notes.feature_note.domain.repository.NoteRepository
-import com.breaktime.notes.feature_note.domain.use_case.DeleteNoteUseCase
-import com.breaktime.notes.feature_note.domain.use_case.GetNotesUseCase
-import com.breaktime.notes.feature_note.domain.use_case.NoteUseCases
+import com.breaktime.notes.feature_note.domain.use_case.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -36,7 +34,9 @@ object AppModule {
     fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
         return NoteUseCases(
             getNotesUseCase = GetNotesUseCase(repository),
-            deleteNoteUseCase = DeleteNoteUseCase(repository)
+            deleteNoteUseCase = DeleteNoteUseCase(repository),
+            addNoteUseCase = AddNoteUseCase(repository),
+            getNoteUseCase = GetNoteUseCase(repository)
         )
     }
 }
